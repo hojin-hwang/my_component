ef4eab39e80d81a8397d8aaf547ed86568dc860c
hojinh <hoinh@gmail.com>
2022-01-25 10:01:25 +0900

    component abs 추가

---
+++ js/component-abs.js
@@ -0,0 +1,15 @@
+class ComponentABS extends HTMLElement{
+    constructor()
+    {
+        super();
+        this.addEventListener('click', this.handleClick);
+        window.addEventListener("message", this.receiveMessage, false);
+        this.message_prefix = Math.random().toString(16).substring(2,8);//내부 명령 접두어
+        this.showComponent = true;
+        this.language = 'ko';
+        if(typeof this._getLanguageSet == 'function' ) this.languageSet = this._getLanguageSet(this.language);
+    }
+    static get observedAttributes() {return ['lang']; }
+    
+    receiveMessage = this.onMessage.bind(this);
+}
